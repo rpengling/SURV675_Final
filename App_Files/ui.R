152a119f45a4f43ab2c403bb896df84ff3e2c3a8
Riki Engling <rengling@umd.edu>
2025-05-15 22:01:44 -0400

    Corrected formatting on ui.R and made the exported report look nicer.

--- App_Files/ui.R
+++ App_Files/ui.R
@@ -47,19 +47,22 @@ shinyUI(
                     tabItem(tabName = "about", 
                             h1("Welcome!"), 
                             h4("This app is designed to let you explore data from the European Value Study's 2017 Wave."),
-                            p("Please use the sidebar menu to navigate through the different pages. Continue reading for more information about the pages."), 
+                            h5("Please use the sidebar menu to navigate through the different pages. Continue reading for more information about the pages."), 
                             
                             h2("Exploration & Regression"), 
                             h4("Here you will find four boxes where you can select different variables and manipulate the graphs, tables, and regression formula."), 
                             
                             h3("Selection Boxes"), 
-                            p("The 'Country' box contains a dropdown where you can select a specific country you are interested in. The default is to show you all countries contain within the dataset. The 'Outcome Variable' box lets you select which of the outcome variables you are interested in investigating. The options are 'GenRol' which represents the attitudes towards traditional gender roles where a higher values reported indicates more positive views on traditional roles, such as believing children suffer if the mother works. The second option, 'Immig, is representative of attitudes towards immigration where higher values reported indicate more positive views towards immigration, such as believing people of the nation should not be given priority over immigrants if jobs are scarce. The 'Control Variable(s)' box lets you select how many control variables you would like considered in the regression calculation. You can select 'Sex' and/or 'Edu', for level of education, or leave the box empty. 'Age' is automatically a part of the regression formula. 'Polynomial Value' also influences the regression formula by changing the number of polynomials of age in the regression, with the options of 1, 2, 3, 4, and 5."), 
+                            h5("The 'Country' box contains a dropdown where you can select a specific country you are interested in. The default is to show you all countries contain within the dataset. The 'Outcome Variable' box lets you select which of the outcome variables you are interested in investigating. The options are 'GenRol' which represents the attitudes towards traditional gender roles where a higher values reported indicates more positive views on traditional roles, such as believing children suffer if the mother works. The second option, 'Immig, is representative of attitudes towards immigration where higher values reported indicate more positive views towards immigration, such as believing people of the nation should not be given priority over immigrants if jobs are scarce. The 'Control Variable(s)' box lets you select how many control variables you would like considered in the regression calculation. You can select 'Sex' and/or 'Edu', for level of education, or leave the box empty. 'Age' is automatically a part of the regression formula. 'Polynomial Value' also influences the regression formula by changing the number of polynomials of age in the regression, with the options of 1, 2, 3, 4, and 5."), 
                             
                             h3("Graphs"), 
-                            p("The 'Exploration' tab depicts the average value reported of the outcome variable of your choosing, based on respondents' age group, sex, and education level. The 'Regression' tab depicts a scatter plot of the the predicted versus the residuals from the regression."), 
+                            h5("The 'Exploration' tab depicts the average value reported of the outcome variable of your choosing, based on respondents' age group, sex, and education level. The 'Regression' tab depicts a scatter plot of the the predicted versus the residuals from the regression."), 
                             
                             h3("Regression Results"), 
-                            p("This section depicts the results from the linear regression, influenced by which control and outcome variables you choose. By default, the regression formula predicts attitudes towards traditional gender roles on age."), 
+                            h5("This section depicts the results from the linear regression, influenced by which control and outcome variables you choose. By default, the regression formula predicts attitudes towards traditional gender roles on age."), 
+                            
+                            h3("Download Report"), 
+                            h5("If you would like to download a copy of the results based on your selected inputs, click to download an HTML file of the graphs and regression table."),
                             
                             h2("Data"), 
                             h4("You will see the display of the full dataset used for this app."), 
